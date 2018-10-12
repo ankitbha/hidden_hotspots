dd60a60c8e3a3c9db5b221212f0eb131d4f31761
Ulzee An <ul1994@gmail.com>
2018-10-12 00:23:00 +0000

    basic lstm net ready

--- gnn.py
+++ gnn.py
@@ -1,5 +1,5 @@
 # ********************************************************************
-# 
+#
 # Implement a neural consisting of CNN and LSTM elements for
 # spatiotemporal pollution forecasting.
 #
@@ -24,13 +24,13 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
-
+from generate_nndatasets import *
 
 if __name__ == '__main__':
     # set random seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
-    
+
     hiddensize = 25
 
     parser = argparse.ArgumentParser(description='LSTM for traffic congestion prediction')
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     # printing arguments
     print(os.linesep + 'Program arguments: ')
     print('Segment: {}, history: {}, hops: {}, ahead: {}, gc: {}'.format(args.segment, args.history, args.hops, args.ahead, args.gc) + os.linesep)
-    
+
     # segname = os.path.splitext(os.path.basename(sys.argv[1]))[0]
     segname = args.segment
 
@@ -56,17 +56,17 @@ if __name__ == '__main__':
 
     if args.gc:
         # *** HARDCODING THIS FOR NOW!! THIS IS NOT CORRECT FOR GENERIC INPUT. ***
-        # 
+        #
         # |   |   |
         # V   V   V
-        # 
+        #
         # We assume that if hop is 1, then given segment has 1 successor
         # and 1 predecessor. If hop is 2, then 2 successors and 2
         # predecessors.
-        
+
         adjmat = None
         weightmat = None
-        
+
         if args.hops == 1:
             adjmat = np.array([[1, 1, 1],
                                [1, 1, 0],
@@ -88,22 +88,22 @@ if __name__ == '__main__':
         else:
             raise Exception("With GC, values supported for \"hops\" is only 1 or 2.")
 
-    
+
     # load data and split into training and testing
     arr = torch.load(infilepath)
 
     if len(arr.shape) == 2:
         arr = np.expand_dims(arr, axis=2)
-    
+
     # 'input' is data from first 60 days and 'target' is predicting
     # next point given previous two points
 
     numsegments = arr.shape[2]
-    
+
     # training
     input = torch.from_numpy(arr[:60, :-1, :])
     target = torch.from_numpy(arr[:60, args.history:, 0])
-    
+
     # test
     test_input = torch.from_numpy(arr[60:, :-1, :])
     test_target = torch.from_numpy(arr[60:, args.history:, 0])
@@ -116,17 +116,17 @@ if __name__ == '__main__':
     else:
         seq = Sequence(args.history, numsegments, hiddensize)
         nnname = 'lstm'
-    
+
     seq.double()
 
     # mean-squared loss criterion works reasonably for estimating real
     # values
     criterion = nn.MSELoss()
-    
+
     # use LBFGS as optimizer since we can load the whole data to train
     optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
     # optimizer = optim.RMSprop(seq.parameters())
-    
+
     numiter = 20
     loss_mat = np.empty((numiter, 2))
 
@@ -135,7 +135,7 @@ if __name__ == '__main__':
     os.makedirs(saverootdir, exist_ok=True)
 
     ys_iter = []
-    
+
     # begin to train
     for i in range(numiter):
         print('STEP: ', i)
@@ -161,7 +161,7 @@ if __name__ == '__main__':
             loss_mat[i,1] = test_loss
             y = pred.detach().numpy()
             ys_iter.append(y)
-        
+
     min_train_loss, min_test_loss = loss_mat.min(axis=0)
     min_train_loss_iter, min_test_loss_iter = loss_mat.argmin(axis=0)
     stat1 = 'min_train_loss = {}, min_train_loss_iter = {}'.format(min_train_loss, min_train_loss_iter + 1)
@@ -172,7 +172,7 @@ if __name__ == '__main__':
     # save the predictions with min test loss
     np.savetxt(os.path.join(saverootdir, '{}-predict-alldays-{}-{}hop-h{}-a{}-stopiter{:02d}.csv'.format(segname, nnname, args.hops, args.history, args.ahead, min_test_loss_iter)), ys_iter[min_test_loss_iter][:,1:], fmt='%f', delimiter=',')
     np.savetxt(os.path.join(saverootdir, '{}-actual-alldays-h{}.csv'.format(segname, args.history)), test_target.numpy()[:,:-1], fmt='%f', delimiter=',')
-    
+
     with open(os.path.join(saverootdir, '{}-losses-{}-{}hop-h{}-a{}.txt'.format(segname, nnname, args.hops, args.history, args.ahead)), 'w') as fout:
         fout.write(stat1 + '\n')
         fout.write(stat2 + '\n')
--- nets.py
+++ nets.py
@@ -4,117 +4,64 @@ import torch.nn as nn
 import numpy as np
 
 class Sequence(nn.Module):
-    def __init__(self, historylen, numsegments, hiddensize):
+    def __init__(self, batchsize, historylen, numsegments, hiddensize):
         super(Sequence, self).__init__()
+        self.batchsize = batchsize
         self.historylen = historylen
         self.numsegments = numsegments
         self.hiddensize = hiddensize
-        self.lstm1 = nn.LSTMCell(historylen * numsegments, hiddensize)
-        self.lstm2 = nn.LSTMCell(hiddensize, hiddensize)
+        self.lstm1 = nn.LSTM(hiddensize, hiddensize, num_layers=2)
+        self.prelstm = nn.Linear(numsegments, hiddensize)
         self.linear = nn.Linear(hiddensize, 1)
 
-    def forward(self, input, future=0):
-        outputs = []
-        
-        batchsize = input.size(0)
-        
-        h_t = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        c_t = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        h_t2 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        c_t2 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
+    def init_lstms(self):
+        # h_0 of shape (num_layers * num_directions, batch, hidden_size):
+        #   tensor containing the initial hidden state for each element in the batch.
+        # c_0 of shape (num_layers * num_directions, batch, hidden_size):
+        #   tensor containing the initial cell state for each element in the batch.
+
+        # FIXME: batchsize == numsegments for now...
+        h_t = torch.zeros(2, self.batchsize, self.hiddensize)
+        c_t = torch.zeros(2, self.batchsize, self.hiddensize)
+        # h_t2 = torch.zeros(1, self.batchsize, self.hiddensize)
+        # c_t2 = torch.zeros(1, self.batchsize, self.hiddensize)
+
         # h_t3 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
         # c_t3 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
 
-        # torch.chunk() splits the input into as many chunks as given
-        # in the argument and returns a tuple of chunks. Each chunk is
-        # a tensor of dim [batchsize x inputsize]
-        numpoints = input.size(1)
-        
-        sindices = np.arange(numpoints - future - self.historylen + 1)
-        pindices = np.arange(self.historylen, numpoints - future + 1)
-        chunkendpoints = zip(sindices, pindices)
-        
-        for i, (starts, startp) in enumerate(chunkendpoints):
-            chunkinput = input[:, starts:startp, :].reshape((batchsize, self.historylen*self.numsegments))
-            h_t, c_t = self.lstm1(chunkinput, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            # h_t3, c_t3 = self.lstm2(h_t2, (h_t3, c_t3)) # adding another layer (let's see what this does...)
-            output = self.linear(h_t2)
-            outputs += [output]
+        return (h_t, c_t)
 
-        # input for future predictions are the predicted outputs thus
-        # far for the segment in focus, but original inputs for all
-        # the other segments in the neighborhood
-        if future > 0:
-            sindices_future = np.arange(numpoints - future - self.historylen + 1, numpoints - self.historylen + 1)
-            pindices_future = np.arange(numpoints - future + 1, numpoints + 1)
-            chunkendpoints_future = zip(sindices_future, pindices_future)
+    def forward(self, input, lstm_states, future=0):
+        (h_t, c_t) = lstm_states
 
-            input_future = input.clone()
-            input_future[:, sindices_future[0]:pindices_future[0], 0] = torch.stack(outputs[-self.historylen:], 1).squeeze(2)
+        x = self.prelstm(input)
 
-            # iteration for future prediction
-            for i, (starts, startp) in enumerate(chunkendpoints_future):
-                chunkinput_future = input_future[:, starts:startp, :].reshape((batchsize, self.historylen*self.numsegments))
-                h_t, c_t = self.lstm1(chunkinput_future, (h_t, c_t))
-                h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-                # h_t3, c_t3 = self.lstm2(h_t2, (h_t3, c_t3))
-                output = self.linear(h_t2)
-                if startp < numpoints:
-                    input_future[:, startp, 0] = output[:,0]
-                outputs += [output]
-        
-        outputs = torch.stack(outputs, 1).squeeze(2)
-        return outputs
+        # Expected input: (histlen x batchsize x dense_t)
+        x, (h_t, c_t) = self.lstm1(x, (h_t, c_t))
 
+        last_x = x[-1, :, :]
+        x = self.linear(last_x)
 
-class SequenceGC(nn.Module):
-    def __init__(self, historylen, numsegments, hiddensize, convmat):
-        super(SequenceGC, self).__init__()
-        self.historylen = historylen
-        self.numsegments = numsegments
-        self.hiddensize = hiddensize
-        self.lstm1 = nn.LSTMCell(historylen*numsegments, hiddensize)
-        self.lstm2 = nn.LSTMCell(hiddensize, hiddensize)
-        self.linear = nn.Linear(hiddensize, 1)
-        self.convmat = convmat
+        return x, (h_t, c_t)
 
-    def forward(self, input, future = 0):
-        outputs = []
-        
-        batchsize = input.size(0)
-        
-        h_t = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        c_t = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        h_t2 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        c_t2 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        # h_t3 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
-        # c_t3 = torch.zeros(batchsize, self.hiddensize, dtype=torch.double)
+if __name__ == '__main__':
+    from torch import optim
 
-        # torch.chunk() splits the input into as many chunks as given
-        # in the argument and returns a tuple of chunks. Each chunk is
-        # a tensor of dim [batchsize x inputsize]
+    # DEMO
 
-        startindices = np.arange(input.size(1))
-        chunkendpoints = zip(startindices, startindices[self.historylen-1:])
-        
-        for i, (beg, end) in enumerate(chunkendpoints):
-            # implement a convolution operation right here!
-            chunkinput = input[:, beg:end+1, :].reshape((batchsize, self.historylen*self.numsegments))
-            chunkchunks = chunkinput.chunk(self.historylen, dim=1)
-            chunkchunks_gc = [torch.from_numpy(np.matmul(self.convmat, inp.numpy().T).T) for inp in chunkchunks]
-            chunkinput_gc = torch.cat(chunkchunks_gc, dim=1)
-            
-            h_t, c_t = self.lstm1(chunkinput_gc, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            # h_t3, c_t3 = self.lstm2(h_t2, (h_t3, c_t3)) # adding another layer (let's see what this does...)
-            output = self.linear(h_t2)
-            outputs += [output]
-        for i in range(future):# if we should predict the future
-            h_t, c_t = self.lstm1(output, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            # h_t3, c_t3 = self.lstm2(h_t2, (h_t3, c_t3))
-            output = self.linear(h_t2)
-            outputs += [output]
-        outputs = torch.stack(outputs, 1).squeeze(2)
-        return outputs
+    model = Sequence(batchsize=2, historylen=5, numsegments=7, hiddensize=25)
+    lstm_states = model.init_lstms()
+
+    # rand_input = torch.randn(2, 7, 5)  # (batchsize x # segs x histlen)
+    rand_input = torch.randn(5, 2, 7, requires_grad=True)  # (histlen x batchsize x  x #segs)
+    print('Input:', rand_input.size())
+    pred, lstm_states = model(rand_input, lstm_states)
+
+    print('Output:', pred.size())
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+    rand_labels = torch.randn(2, 1)  # (histlen x batchsize x  x #segs)
+    criterion = nn.MSELoss()(rand_labels, pred)
+    criterion.backward()
+    optimizer.step()
