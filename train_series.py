e13e8b743c4f1a15dfdeecf58d77d7398d15450a
Ulzee An <ul1994@gmail.com>
2018-10-15 20:35:09 +0000

    better plot outputs

--- train_series.py
+++ train_series.py
@@ -23,14 +23,17 @@ from random import shuffle
 from torch.autograd import Variable
 from tensorboardX import SummaryWriter
 
-def plot_preview(imgname, segdef, target, segments, predictions, loss, histlen, criterion):
+def plot_preview(epoch, segdef, target, segments, predictions, loss, histlen, criterion, norm=100.0):
+    imgname = 'preview/pred_%d_epoch_%d.png' % (args.target, epoch)
+    currname = 'pred_%d_best.png' % (args.target)
+
     legend = []
     plt.figure(figsize=(14, 7))
     for sii, seg in enumerate(segments):
         if sii != target:
-            pl = plt.plot(seg*100, color='#CCCCCC')
+            pl = plt.plot(seg*norm, color='#CCCCCC')
             legend.append((pl, segdef['locations'][sii][0]))
-    pl = plt.plot(segments[target, :]*100, color='C0')
+    pl = plt.plot(segments[target, :]*norm, color='C0')
     legend.append((pl, segdef['locations'][target][0] + ' (target)'))
 
     # mean of all other measurements at time tt
@@ -39,11 +42,11 @@ def plot_preview(imgname, segdef, target, segments, predictions, loss, histlen, 
         if sii == target: continue
         avgseries += seg
     avgseries /= (segments.shape[0]-1)
-    pl = plt.plot(avgseries*100, color='C2')
+    pl = plt.plot(avgseries*norm, color='C2')
     legend.append((pl, 'avg. of non-target'))
     avgloss = 0
     for tii, val in enumerate(avgseries):
-        trueval = segments[target, tii]*100
+        trueval = segments[target, tii]
         loss_tensor = criterion(torch.tensor(val), torch.tensor(trueval))
         avgloss += loss_tensor.cpu().numpy()
     avgloss /= len(avgseries)
@@ -51,14 +54,17 @@ def plot_preview(imgname, segdef, target, segments, predictions, loss, histlen, 
     # continuous lstm predictions
     xs = list(range(len(predictions)))
     assert len(xs) == len(predictions)
-    pl = plt.plot(predictions, color='C1')
+    pl = plt.plot(np.array(predictions)*norm, color='C1')
     legend.append((pl, 'target preds.'))
 
-    plt.gca().set_title('Pred Loss: %.3f    Avg Loss: %.3f' % (loss, avgloss))
+    plt.gca().set_title('Pred MSE: %.3f    Avg MSE: %.3f' % (
+        loss*norm**2,
+        avgloss*norm**2))
     plts, lbls = zip(*legend)
     plts = [pl[0] for pl in plts] # ?? actual ref is first elem?
     plt.legend(plts, lbls)
     plt.savefig(imgname, bbox_inches='tight')
+    plt.savefig(currname, bbox_inches='tight')
     plt.close()
 
 if __name__ == '__main__':
@@ -69,7 +75,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='LSTM for traffic congestion prediction')
     parser.add_argument('--history', type=int, required=True, help='Length of history')
     parser.add_argument('--hidden', type=int, default=25)
-    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--batch', type=int, default=32)
     parser.add_argument('--epochs', type=int, default=4)
     parser.add_argument('--segment', type=int, required=True, help='Segment to train on; (-1) for all; check configs...')
@@ -89,11 +95,16 @@ if __name__ == '__main__':
     numsegments = len(use_segment['locations'])
     (train_data, test_data), metadata = create_dataset(use_segment)
 
-    model = Series(
-        batchsize=args.batch,
-        historylen=args.history,
-        numsegments=numsegments,
-        hiddensize=args.hidden).to(device).double()
+    if args.load is not None:
+        print(' Loading:', args.load)
+        model = torch.load(args.load)
+    else:
+        model = Series(
+            batchsize=args.batch,
+            historylen=args.history,
+            numsegments=numsegments,
+            hiddensize=args.hidden).to(device).double()
+
 
     # mean-squared loss criterion works reasonably for estimating real
     # values
@@ -144,13 +155,13 @@ if __name__ == '__main__':
             sys.stdout.write('[E:%d/%d] B:%d/%d  loss: %.2f          \r' % (
                 eii+1, args.epochs,
                 bii//args.batch+1, numbatches,
-                loss.item(),
+                loss.item() * 100.0**2,
             ))
             sys.stdout.flush()
 
-        model.eval()
-        lossavg = 0
 
+        model.eval()
+        series_loss = 0
         continuous = []
         lstm_state = model.init_lstms(device=device, batch=1)
         for ind in range(test_data.shape[1]):
@@ -167,23 +178,22 @@ if __name__ == '__main__':
             preds, lstm_state = model(batch_seq, lstm_state)
 
             predval = preds.detach().squeeze().cpu().numpy()
-            continuous.append(predval*100)
+            continuous.append(predval)
 
             loss = criterion(preds, batch_lbls)
-            lossavg += loss.detach().cpu().numpy()
-        lossavg /= (test_data.shape[1] - seqlen)
-        log.add_scalar('test/loss', lossavg, n_iter)
-        print('\n   Testing Loss:  %.3f' % lossavg)
-
-        # fname = 'pred_%d.png' % n_iter
+            series_loss += loss.detach().cpu().numpy()
+        series_loss /= (test_data.shape[1] - seqlen)
+        log.add_scalar('test/loss', series_loss, n_iter)
+        print('\n   Testing Loss:  %.3f' % (series_loss * 100.0**2))
 
         if eii % 10 == 0:
-            plot_preview('preview/pred_seriescont_%d.png' % n_iter,
+            plot_preview(
+                eii,
                 use_segment,
                 args.target,
                 test_data,
                 continuous,
-                lossavg,
+                series_loss,
                 args.history,
                 criterion)
 
