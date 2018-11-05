e22f69856c7855a1c11e829d3557fe92fcd5cb80
Ulzee An <ul1994@gmail.com>
2018-11-05 16:44:47 +0000

    adjusted training script

--- joint.sh
+++ joint.sh
@@ -6,5 +6,6 @@ do
 	# echo $targ
 	# python3 train_series.py --segment -1 \
 	# 	--target $targ --lr 0.00001 --history 32 --epochs 200 --batch 96 --hidden 512
-	python3 train_series.py --segment -2 --target $targ --lr 0.00005 --history 64 --epochs 200 --batch 64 --hidden 128
+	python3 train_series.py --segment -2 --target $targ --lr 0.00005 --history 8 \
+		 --epochs 400 --batch 64 --hidden 128
 done
