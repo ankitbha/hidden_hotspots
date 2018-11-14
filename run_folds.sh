fae39edc002f33cdfd075c43985594ed6763de34
Ulzee An <ul1994@gmail.com>
2018-11-14 20:11:58 +0000

    updated fold training

--- run_folds.sh
+++ run_folds.sh
@@ -5,7 +5,7 @@ do
 	for fold in {0..8}
 	do
 		python3 train_folds.py --mode $1 --fold $fold --target $targ \
-			--lr 0.000005 --history 32                  \
+			--lr 0.000005 --history 32 --stride 2                  \
 			--epochs 120 --batch 32 --hidden 256
 	done
 done
