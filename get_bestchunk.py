c025fee29666f580bc666bddc7e5bb4cd627994f
Shiva R. Iyer <sri223@nyu.edu>
2019-02-15 16:24:54 -0500

    Return chunks based on cutoff for valid percent

--- get_bestchunk.py
+++ get_bestchunk.py
@@ -1,6 +1,15 @@
+import sys
 import glob
 
 flist = glob.iglob('validcounts_cutoff*_*M.txt')
+try:
+    percent_cutoff = round(float(sys.argv[1]))
+    if percent_cutoff < 0 or percent_cutoff >= 100:
+        raise ValueError
+except (ValueError, IndexError):
+    print('Enter valid percentage value in [0,100)')
+    print('Usage: python {} <percent>'.format(sys.argv[0]))
+    sys.exit(-1)
 
 validpercent_list = []
 remove_list_list = []
@@ -16,13 +25,13 @@ for fpath in flist:
             else:
                 parts = line.rsplit(',', 1)
                 percent = float(parts[1].strip()[:-1])
-                if percent >= 50:
+                if percent >= percent_cutoff:
                     validpercent_list.append(percent)
                     remove_list_list.append(remove_list)
                     params_list.append(parts[0].strip()[5:])
                     cutoff_list.append(cutoff)
 
-with open('validcounts_bestchunks.txt', 'w') as fout:
+with open('validcounts_bestchunks_{:02d}.txt'.format(percent_cutoff), 'w') as fout:
     for tup in zip(validpercent_list, remove_list_list, params_list, cutoff_list):
         validpercent, remove_list, params, cutoff = tup
         fout.write('\nMax valid percent: {}\n'.format(validpercent))
