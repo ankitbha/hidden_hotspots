81fcffca6598fcc7bb0c4fea7a1a62690d1dc987
Shiva R. Iyer <sri223@nyu.edu>
2019-03-09 16:06:19 -0500

    Change train-test split to 90:10
    Also update test script to run for govt data and pm10.

--- test_knn_batch.sh
+++ test_knn_batch.sh
@@ -3,7 +3,7 @@
 for kval in {1..10}
 do
     # run testing and plotting script
-    python test_knn.py $kval v1
-    python test_knn.py $kval v2
+    python test_knn.py $1 $2 $kval v1 --split 0.9
+    python test_knn.py $1 $2 $kval v2 --split 0.9
 done
 
--- train_knn_batch.sh
+++ train_knn_batch.sh
@@ -3,7 +3,7 @@
 for kval in {1..10}
 do
     # run training for each value of K
-    python train_knn.py $1 $2 $kval v1 --batch 96 --epochs 20 --max-batches 30 --yes
-    python train_knn.py $1 $2 $kval v2 --batch 96 --epochs 20 --max-batches 30 --yes
+    python train_knn.py $1 $2 $kval v1 --batch 96 --epochs 20 --max-batches 30 --split 0.9 --yes
+    python train_knn.py $1 $2 $kval v2 --batch 96 --epochs 20 --max-batches 30 --split 0.9 --yes
 done
 
