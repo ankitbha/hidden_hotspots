5339c83d8ee4b9d41542919d11c51bd80d450260
Shiva R. Iyer <sri223@nyu.edu>
2019-06-15 17:38:32 -0400

    Archive very old scripts into _archive

--- gov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for targ in {0..10}
-do
-	# echo $targ
-	python3 train_series.py --segment -1 \
-		--target $targ --lr 0.00001 --history 32 --epochs 200 --batch 96 --hidden 512
-done
