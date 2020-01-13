8fee69eb41374896544734ea20f83e0231bd1178
Shiva R. Iyer <sri223@nyu.edu>
2020-01-13 18:17:32 -0500

    Add more options for plotting freq buckets

--- plot.py
+++ plot.py
@@ -6,34 +6,46 @@ import glob
 import os
 
 
-def plot_cluster_freqbuckets(inpdir, sensor, thres, n_clusters):
+def plot_cluster_freqbuckets(inpdir, n_clusters, thres_cpd=3, sensor='pm25', min_points=1000, monitor_id=None, save=False):
     import glob
     from sklearn import cluster
 
     assert 2 <= n_clusters <= 10
 
-    flist = glob.iglob(os.path.join(inpdir, '*_thres{:02d}CPD_lowpass.csv'.format(thres)))
+    if monitor_id is None:
+        flist = glob.iglob(os.path.join(inpdir, '*_{}_*_freqbuckets_filter_thres{:02d}CPD_lowpass.csv'.format(sensor, thres_cpd)))
+    else:
+        flist = glob.iglob(os.path.join(inpdir, '{}_{}_*_freqbuckets_filter_thres{:02d}CPD_lowpass.csv'.format(monitor_id, sensor, thres_cpd)))
     X = []
     n_lines = []
     
     for ind, fname in enumerate(flist):
         nameparts = os.path.basename(fname).rsplit('_', 6)
         ts = np.loadtxt(os.path.join(inpdir, '{}_{}_{}_T.csv'.format(nameparts[0], sensor, nameparts[2])), delimiter=',', usecols=[1], skiprows=1)
-        if ts.shape[0] > 200:
-            # only consider those portions that have more than 200
-            # points
+        if ts.shape[0] > min_points:
+            # only consider those portions that have more than a
+            # threshold no of points
             X.append(np.loadtxt(fname, delimiter=',', skiprows=2, usecols=[1]))
             n_lines.append(ts.shape[0])
+
+    if len(X) == 0:
+        return
 
     freqbuckets = np.loadtxt(fname, delimiter=',', skiprows=2, usecols=[0])
     
     X = np.asarray(X)
-    
-    centroids, labels, _ = cluster.k_means(X, n_clusters)
+
+    if X.shape[0] > n_clusters:
+        centroids, labels, _ = cluster.k_means(X, n_clusters)
+    else:
+        n_clusters = X.shape[0]
+        centroids = X
+        labels = np.arange(X.shape[0])
 
     print('Count of data points in each cluster:', np.bincount(labels))
 
-    plotdims_dict = {2: (1,2),
+    plotdims_dict = {1: (1,1),
+                     2: (1,2),
                      3: (2,2),
                      4: (2,2),
                      5: (2,3),
@@ -46,7 +58,7 @@ def plot_cluster_freqbuckets(inpdir, sensor, thres, n_clusters):
     rowdim, coldim = plotdims_dict[n_clusters][0], plotdims_dict[n_clusters][1]
 
     # (1) plot only the centroids
-    fig1, axs1 = plt.subplots(rowdim, coldim,
+    fig1, axs1 = plt.subplots(rowdim, coldim, squeeze=False,
                             sharex=True, sharey=True,
                             subplot_kw=dict(xlabel='CPD', ylabel='Bin prob'),
                             figsize=(rowdim*3, coldim*4))
@@ -55,7 +67,7 @@ def plot_cluster_freqbuckets(inpdir, sensor, thres, n_clusters):
 
     fig1.suptitle('All cluster centroids')    
 
-    fig2, axs2 = plt.subplots(rowdim, coldim,
+    fig2, axs2 = plt.subplots(rowdim, coldim, squeeze=False,
                             sharex=True, sharey=True,
                             subplot_kw=dict(xlabel='CPD', ylabel='Bin prob'),
                             figsize=(rowdim*3, coldim*4))
@@ -73,8 +85,23 @@ def plot_cluster_freqbuckets(inpdir, sensor, thres, n_clusters):
         axs2.flat[ind].set_title('n_points {} to {}'.format(n_points_min[ind], n_points_max[ind]))
     
     fig2.suptitle('All data points')
+    
+    if save:
+        if monitor_id is None:
+            save_dir = 'figures/cluster_freqbuckets'
+            save_suffix = 'K{:02d}_{}_filter_thres{:02d}_all'.format(n_clusters, sensor, thres_cpd)
+        else:
+            save_dir = 'figures/cluster_freqbuckets/{}'.format(monitor_id)
+            save_suffix = 'K{:02d}_{}_filter_thres{:02d}_{}'.format(n_clusters, sensor, thres_cpd, monitor_id)
 
-    plt.show()
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        fig1.savefig(os.path.join(save_dir, 'centroids_' + save_suffix + '.png'))
+        fig2.savefig(os.path.join(save_dir, 'clusters_' + save_suffix + '.png'))
+    else:
+        plt.show()
+
+    plt.close('all')
     
     return
     
@@ -219,6 +246,12 @@ def plot_scatter_1nn(mid, source, sensor):
 
 if __name__ == '__main__':
 
+    save_response = input('Save all figures? [y] ')
+    if save_response.strip().lower() == 'y':
+        save_response = True
+    else:
+        save_response = False
+
     # mids_list = ['CBC7', 'A9BE', 'C0A7', '72CA', 'BC46', '20CA', 'EAC8', '113E', 'E8E4', '603A']
     mids1_list = np.loadtxt('data/kaiterra/kaiterra_fieldeggid_locations.csv', delimiter=',', dtype=str, skiprows=1, usecols=[0])
     mids2_list = np.loadtxt('data/govdata/govdata_locations.csv', delimiter=',', dtype=str, skiprows=1, usecols=[0])
@@ -231,12 +264,19 @@ if __name__ == '__main__':
 
     # plot_bars_mid_quant_bestperf(mids_list, 'kaiterra', 'pm25', 'v2')
     
-    for mid in mids1_list:
-        print(mid)
-        plot_scatter_1nn(mid, 'kaiterra', 'pm25')
-        #assert False
-        
+    # for mid in mids1_list:
+    #     print(mid)
+    #     plot_scatter_1nn(mid, 'kaiterra', 'pm25')
     
-    for mid in mids2_list:
-        print(mid)
-        plot_scatter_1nn(mid, 'govdata', 'pm25')
+    # for mid in mids2_list:
+    #     print(mid)
+    #     plot_scatter_1nn(mid, 'govdata', 'pm25')
+
+    from itertools import chain
+    for mid in chain(mids1_list, mids2_list):
+        # print(mid)
+        plot_cluster_freqbuckets('figures/freq_components/location_wise/', 3, monitor_id=mid, save=save_response)
+        # flist = glob.glob(os.path.join('figures/freq_components/location_wise/', '{}_pm25_*_freqbuckets_filter_thres03CPD_lowpass.csv'.format(mid)))
+        # flist.sort()
+        # print(mid, len(flist))
+        # print(flist)
