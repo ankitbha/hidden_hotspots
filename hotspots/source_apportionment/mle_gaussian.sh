9c6bb898291f137978fd0a9fea61c813a7835506
Ankit Bhardwaj <bhardwaj.ankit275@gmail.com>
2023-01-30 11:58:01 -0500

    Dispersion modeling

---
+++ hotspots/source_apportionment/mle_gaussian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=18:00:00
+#SBATCH --mem=32GB
+#SBATCH --job-name=mle_gaussian
+#SBATCH --output=slurm_mle_%j.out
+
+conda activate /scratch/ab9738/cctv_pollution/env/;
+export PATH=/scratch/ab9738/cctv_pollution/env/bin:$PATH;
+cd /scratch/ab9738/pollution_with_sensors/hotspots/source_apportionment/
+python mle_gaussian.py
