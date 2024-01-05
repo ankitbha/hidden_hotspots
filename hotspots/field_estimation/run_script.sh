23186ba060431bf5fce8af16c930d04b89926193
Ankit Bhardwaj <bhardwaj.ankit275@gmail.com>
2024-01-04 20:51:11 -0500

    additional policy recs, maps and figures for the paper

--- hotspots/field_estimation/run_script.sh
+++ hotspots/field_estimation/run_script.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=02:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=32GB
 
 conda activate /scratch/ab9738/cctv_pollution/env/;
 export PATH=/scratch/ab9738/cctv_pollution/env/bin:$PATH;
 cd /scratch/ab9738/pollution_with_sensors/hotspots/field_estimation/
-python interpolation_ext.py 0.1
-python interpolation_ext.py 0.2
-python interpolation_ext.py 0.3
-python interpolation_ext.py 0.4
-python interpolation_ext.py 0.5
+python heatmaps_longitudinal.py
